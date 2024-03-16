aae031104896aec48d818ae78da89939949b5b3e
sirmahdimir <sirmahdimir@gmail.com>
2024-03-16 14:51:54 +0330

    counter model and template were created

--- counter_proj/counter_app/models.py
+++ counter_proj/counter_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Digit(models.Model):
+    num = models.IntegerField(default=0)
+
+    def decreasment(self):
+        self.num -= 1
+        self.save()
+
+    def reset(self):
+        self.num = 0
+        self.save()
+
+    def increasment(self):
+        self.num += 1
+        self.save()
+
+    def __str__(self):
+        return f'Digit Is : {self.num}'
+        
--- counter_proj/counter_app/views.py
+++ counter_proj/counter_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Digit
 
 # increase , decrease and reset counter:
 def counter(request):
-    pass
+    digit = Digit.objects.first()
+    
+
+    if request.method == 'POST':
+
+        if "decreasment" in request.POST:
+            digit.decreasment()
+
+        if "reset" in request.POST:
+            digit.reset()
+
+        if "increasment" in request.POST:
+            digit.increasment()
+
+    return render (request , 'counter_app/counter_page.html', context={'num' : digit})
+
